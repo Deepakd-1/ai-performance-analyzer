b77c086724a498e302a0c45e627e42f04aac76ba
Deepakd-1 <deepanshu2024@lpu.in>
2025-12-12 17:27:51 +0530

    Liner Regression addition

--- analyzer.py
+++ analyzer.py
@@ -2,3 +2,9 @@ import psutil
 import time
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+cpu_history = []
+ram_history = []
+
+model_cpu = LinearRegression()
+model_ram = LinearRegression()
