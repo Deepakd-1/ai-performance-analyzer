e196c8b4a012a713871c62408d411e10e757b9d7
Deepakd-1 <deepanshu2024@lpu.in>
2025-12-12 17:28:33 +0530

    Future Prediction feature

--- analyzer.py
+++ analyzer.py
@@ -8,3 +8,14 @@ ram_history = []
 
 model_cpu = LinearRegression()
 model_ram = LinearRegression()
+
+def predict_future(history, model):
+    if len(history) < 5:
+        return None  # Not enough data
+    
+    X = np.arange(len(history)).reshape(-1, 1)
+    y = np.array(history)
+
+    model.fit(X, y)
+    future = model.predict([[len(history) + 5]])  # Predict 5 sec ahead
+    return round(future[0], 2)
